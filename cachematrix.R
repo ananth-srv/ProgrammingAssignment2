3793b4d8939c110458aced8e32ca84149253bc05
Vijay <vijayananth86@gmail.com>
2016-09-26 00:48:06 -0700

    Committing the Cache Matrix code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below two functions help us understanding the concept of caching in R
 
-## Write a short comment describing this function
+## This function is the base which sets up all the necessary functions and variables required for 
+## setting up caching and retrieving the data
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function retrieves the cached value if it is already present else it calculates the 
+## inverse of the matrix and stores the data in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  message("calculating inverse..")
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
